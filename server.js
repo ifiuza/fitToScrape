4b89ff0bcda10e96854ca32bdc37c991e02e1922
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-09-19 21:52:33 -0600

    adding database

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/Homework14", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Homework14"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
